97aff435fb9ee85f7188bd9a74e4d4d0c2a05142
SotrydnikSee <47135170+SotrydnikSee@users.noreply.github.com>
2019-06-27 13:04:33 +0400

    New

---
+++ app/routes.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os, datetime
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request, send_from_directory,render_template_string
+from flask_user import current_user, login_required, roles_required, UserManager, UserMixin
+from werkzeug.utils import secure_filename
+from werkzeug.urls import url_parse
+from app.models import User, UserRoles, Role
+
+@app.route('/')
+def home_page():
+    return render_template_string("""
+            {% extends "flask_user_layout.html" %}
+            {% block content %}
+                <h2>{%trans%}Home page{%endtrans%}</h2>
+                <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
+                <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
+                <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
+                <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
+                <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
+                <p><a href={{ url_for('users_table') }}>{%trans%}Users table{%endtrans%}</a></p>
+                <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
+            {% endblock %}
+            """)
+
+@app.route('/users_table')
+@roles_required('Admin')
+def users_table():
+    users = User.query.all()
+    return render_template('table.html', users=users)
+
+@app.route('/members')
+@login_required    # Use of @login_required decorator
+def member_page():
+    return render_template_string("""
+            {% extends "flask_user_layout.html" %}
+            {% block content %}
+                <h2>{%trans%}Members page{%endtrans%}</h2>
+                <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
+                <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
+                <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
+                <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
+                <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
+                <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
+            {% endblock %}
+            """)
+
+    # The Admin page requires an 'Admin' role.
+@app.route('/admin')
+@roles_required('Admin')    # Use of @roles_required decorator
+def admin_page():
+    return render_template_string("""
+            {% extends "flask_user_layout.html" %}
+            {% block content %}
+                <h2>{%trans%}Admin Page{%endtrans%}</h2>
+                <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
+                <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
+                <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
+                <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / Password1)</p>
+                <p><a href={{ url_for('admin_page') }}>{%trans%}Admin Page{%endtrans%}</a> (role_required: admin@example.com / Password1')</p>
+                <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
+            {% endblock %}
+            """)
